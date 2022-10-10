37a216e5b037e5cdcecc45be853709d819d28a45
PavelSuchkov <suchkovp@gmail.com>
2022-10-10 18:23:03 +0300

    added new DB & authentfication flow

---
+++ Models/AppIdentityDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.DependencyInjection;
+using WorkingWithVisualStudio;
+
+namespace SportsStore.Models
+{
+
+    public class AppIdentityDbContext : IdentityDbContext<IdentityUser>
+    {
+
+        public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options): base(options) { }
+    }
+
+    public class AppIdentityDbContextFactory : IDesignTimeDbContextFactory<AppIdentityDbContext>
+    {
+        public AppIdentityDbContext CreateDbContext(string[] args) =>
+            Program.BuildWebHost(args).Services.GetRequiredService<AppIdentityDbContext>();
+    }
+}
